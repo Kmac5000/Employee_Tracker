d1baf35106d3b3105e11596b05ef5a6d9e4115ee
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-06-09 09:47:45 -0600

    starting prompt, json package corrected

--- server.js
+++ server.js
@@ -4,3 +4,32 @@ const inquirer = require("inquirer");
 const express = require("express");
 const mysql = require("mysql2");
 const cTable = require("console.table");
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   DB_USER,
+//   DB_PASSWORD,
+//   DB_NAME,
+// });
+
+function startProgram() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What do you need to see?",
+      name: "choice",
+      choices: [
+        "View All Employees?",
+        "View All Employee's By Roles?",
+        "View all Emplyees By Deparments",
+        "Update Employee",
+        "Add Employee?",
+        "Add Role?",
+        "Add Department?",
+      ],
+    },
+  ]);
+}
+
+startProgram();
