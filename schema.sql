d7a8384b5c68c33a0d9adf0c677a4b10d92f418f
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-06-10 08:26:25 -0600

    table updates

--- schema.sql
+++ schema.sql
@@ -14,6 +14,9 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 
@@ -23,4 +26,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 );
